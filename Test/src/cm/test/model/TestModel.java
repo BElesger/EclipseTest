0a37ed146b0c5189571914f2fad7a482b10c035d
Elesger <yorgunkasva@gmail.com>
2017-10-27 22:20:56 +0400

    adding fields

--- Test/src/cm/test/model/TestModel.java
+++ Test/src/cm/test/model/TestModel.java
@@ -1,5 +1,48 @@
 package cm.test.model;
 
 public class TestModel {
+	private long id;
+	private String name;
+	private String surname;
+
+	@Override
+	public String toString() {
+		return "TestModel [id=" + id + ", name=" + name + ", surname=" + surname + "]";
+	}
+
+	public TestModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestModel(long id, String name, String surname) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
 }
